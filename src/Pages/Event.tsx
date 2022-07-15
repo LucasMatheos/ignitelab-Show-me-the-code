716f4ea99acf44b1ece7a9cf7aae2e98116dfaf2
Lucas Brito <lucasm_brito@hotmail.com>
2022-07-15 20:24:03 -0300

    add classnames lib and add show me the code img

--- src/Pages/Event.tsx
+++ src/Pages/Event.tsx
@@ -2,20 +2,24 @@ import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
-
-
+import ShowMeTheCode from "../assets/smtc.jpg";
 
 export function Event() {
-
-  const {slug} = useParams<{slug:string}>()
+  const { slug } = useParams<{ slug: string }>();
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1 ">
-        { slug 
-        ? <Video lessonSlug={slug}/> 
-        : <div className="flex-1"/>}
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1">
+            <div className="bg-black flex justify-center h-screen">
+              <img src={ShowMeTheCode} alt="Show Me The Code Image" />
+            </div>
+          </div>
+        )}
         <Sidebar />
       </main>
     </div>
