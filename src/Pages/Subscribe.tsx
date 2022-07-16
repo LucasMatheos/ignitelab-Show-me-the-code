b86c6987a2b7f27e3f5972f32da8b6aa82189d47
Lucas Brito <lucasm_brito@hotmail.com>
2022-07-16 14:42:55 -0300

    Responsive Subscriber page

--- src/Pages/Subscribe.tsx
+++ src/Pages/Subscribe.tsx
@@ -27,23 +27,23 @@ export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <img src={ReactIcon} alt="React Icon" className="fixed -z-10" />
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex flex-col items-center justify-between  mt-10 mx-auto sm:flex-row sm:mt-20">
+        <div className="max-w-[640px] flex flex-col items-center mb-8 sm:mb-0 sm:items-start">
           <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma{" "}
-            <strong className="text-blue-500">aplicação completa</strong>, do
+          <h1 className="text-[2rem] leading-tight text-center p-6 sm:text-left sm:text-[2.5rem] sm:px-0 sm:mt-8">
+            Construa uma
+            <strong className="text-blue-500"> aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React</strong>
           </h1>
 
-          <p className="mt-4 ">
+          <p className=" px-6 text-center sm:text-left sm:px-0 ">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="w-full p-8 bg-gray-700 border border-gray-500 rounded sm:w-auto">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
@@ -73,7 +73,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-img.png" alt="Code Image" className="mt-10" />
+      <img src="/src/assets/code-img.png" alt="Code Image" className="mt-4 sm:mt-10" />
     </div>
   );
 }
