d9038e5c3d8db0fe13501c37ff6c9d2629db97e1
Lucas Brito <lucasm_brito@hotmail.com>
2022-07-16 13:14:25 -0300

    added discord link to anchor

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -67,7 +67,8 @@ export function Video({ lessonSlug }: VideoProps) {
 
           <div className="flex flex-col gap-4">
             <a
-              href=""
+              href="https://discord.com/invite/rocketseat"
+              target="_blank"
               className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
             >
               <DiscordLogo size={24} />
